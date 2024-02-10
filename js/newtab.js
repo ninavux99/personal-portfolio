5050b166e937b75ff7b1cd3bf1edb188facbb24f
ninavux99 <123576465+ninavux99@users.noreply.github.com>
2024-02-10 15:07:20 -0500

    ok

---
+++ js/newtab.js
@@ -0,0 +1,7 @@
+window.addEventListener ("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    document.querySelector ('body').style.background
+    }
+    else {
+    document.querySelector ('body').style.background
+    } })
