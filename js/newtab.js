8cf66c4fe706ff4488fcaa3a870557605aa03fc7
ninavux99 <123576465+ninavux99@users.noreply.github.com>
2024-02-13 11:34:58 -0500

    okie last

--- js/newtab.js
+++ js/newtab.js
@@ -4,5 +4,14 @@ window.addEventListener ("scroll", () => {
     }
     else {
     document.querySelector ('body').style.background
-    } })
+    } 
 
+    const header = document.querySelector('header');
+    if (window.scrollY>0){
+        header.classList.add('scrolled');
+    }else {
+        header.classList.remove('scrolled');
+    }
+})
+
+   
