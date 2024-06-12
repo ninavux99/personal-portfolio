ce51fff6234d3f2a32f056312b3449171c3329e7
Ninavux <123576465+ninavux99@users.noreply.github.com>
2024-06-12 18:35:26 +0000

    main.js 1

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ import './style.css'
 import * as THREE from 'three';
 //Scene container
 const scene = new THREE.Scene();
-const camera = new THREE.per
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+//Render == draw
+const renderer = new THREE.WebGLRender({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
